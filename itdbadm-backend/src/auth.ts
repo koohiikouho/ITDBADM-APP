064c6f4c937b4658a803dade6c550cca59d8c96e
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-11-17 02:05:14 +0800

    feat: add signup route

--- itdbadm-backend/src/auth.ts
+++ itdbadm-backend/src/auth.ts
@@ -1,52 +1,57 @@
 import { Elysia, t } from "elysia";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { dbPool } from "./db";
 import { jwt } from "@elysiajs/jwt";
 
-export const authRoutes = new Elysia ({ prefix: '/auth' })
-.use(
+export const authRoutes = new Elysia({ prefix: "/auth" })
+  .use(
     jwt({
-        name: 'jwt',
-        secret: process.env.JWT_SECRET as string,
-        exp: '1d'
+      name: "jwt",
+      secret: process.env.JWT_SECRET as string,
+      exp: "1d",
     })
-)
+  )
 
-.post('/signup', async({ body, jwt, set }) => {
-    // check if user already exists
-    const { lastName, email, password, currency, userType } = body;
+  .post(
+    "/signup",
+    async ({ body, jwt, set }) => {
+      // check if user already exists
+      const { lastName, email, password, currency, userType } = body;
 
-    const [rows] = await dbPool.query(
-        'SELECT user_id FROM users WHERE username = ?', [
-            lastName,
-        ]);
-    if ((rows as any[]).length > 0) {
+      const [rows] = await dbPool.query(
+        "SELECT user_id FROM users WHERE username = ?",
+        [lastName]
+      );
+      if ((rows as any[]).length > 0) {
         set.status = 409;
-        return { message: 'Username already exists' };
-    }
+        return { message: "Username already exists" };
+      }
 
-
-
-    const hashed = await bcrypt.hash(password, 10);
-    const [result] = await dbPool.query(
-        'INSERT INTO users (username, email, password_hashed, currency_id, role_id) VALUES (?, ?, ?, ?, ?)',
+      const hashed = await bcrypt.hash(password, 10);
+      const [result] = await dbPool.query(
+        "INSERT INTO users (username, email, password_hashed, currency_id, role_id) VALUES (?, ?, ?, ?, ?)",
         [lastName, email, hashed, currency, userType]
-    );
+      );
 
-    const userId = (result as any).insertId;
+      const userId = (result as any).insertId;
 
-    const token = await jwt.sign({ id: userId });
+      const token = await jwt.sign({ id: userId });
 
-    return { message: 'User registered successfully', token };
-
-},
-{
-    body: t.Object({
+      return { message: "User registered successfully", token };
+    },
+    {
+      body: t.Object({
         lastName: t.String(),
         email: t.String(),
         password: t.String(),
-        currency: t.Number(),
-        userType: t.Number()
-    })
-}
-)
+        currency: t
+          .Transform(t.Union([t.Number(), t.String()]))
+          .Decode((value) => Number(value))
+          .Encode((value) => value),
+        userType: t
+          .Transform(t.Union([t.Number(), t.String()]))
+          .Decode((value) => Number(value))
+          .Encode((value) => value),
+      }),
+    }
+  );
