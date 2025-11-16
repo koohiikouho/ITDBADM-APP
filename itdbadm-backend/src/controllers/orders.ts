a8eff60aea5aa5ee0560f1eccf52cdd32eaed83d
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-11-17 03:40:43 +0800

    chore: change orders.ts

--- itdbadm-backend/src/controllers/orders.ts
+++ itdbadm-backend/src/controllers/orders.ts
@@ -1,85 +1,81 @@
-import { Elysia, t } from 'elysia';
-import { dbPool } from '../db'; 
-import mysql from 'mysql2/promise';
+import { Elysia, t } from "elysia";
+import { dbPool } from "../db";
+import mysql from "mysql2/promise";
 import { jwt } from "@elysiajs/jwt";
 
+export const ordersController = new Elysia({ prefix: "/orders" })
 
-export const ordersController = new Elysia({ prefix: '/orders' })
+  // GET /orders
+  .get("/", async ({ set }) => {
+    try {
+      const query = `SELECT * FROM orders`;
 
-// GET /orders
-    .get('/', async ({ set }) => {
-        try {
+      const [rows] = await dbPool.execute<mysql.RowDataPacket[]>(query);
 
-            const query = `SELECT * FROM orders`;
+      if (rows.length === 0) {
+        set.status = 200;
+        return { message: "No orders at the moment." };
+      }
 
-            const [rows] = await dbPool.execute<mysql.RowDataPacket[]>(query);
+      const orderList = rows.map((row) => ({
+        id: row.order_id,
+        user_id: row.user_id,
+        order_date: row.order_date,
+        status: row.status,
+        date_fulfilled: row.date_fulfilled,
+        price: row.price,
+        offer_id: row.offer_id,
+        description: row.description,
+      }));
 
-            if (rows.length === 0) {
-                set.status = 200;
-                return { message: "No orders at the moment." };
-            }
+      set.status = 200;
+      return orderList;
+    } catch (error) {
+      console.error("Error fetching all orders:", error);
+      set.status = 500;
+      return { message: "Internal Server Error while retrieving order list." };
+    }
+  })
 
-            const orderList = rows.map((row) => ({
-                id: row.order_id,
-                user_id: row.user_id,
-                order_date: row.order_date,
-                status: row.status,
-                date_fulfilled: row.date_fulfilled,
-                price: row.price,
-                offer_id: row.offer_id,
-                description: row.description,
-            }));
+  // GET /orders/:id
 
-            set.status = 200;
-            return orderList;
+  .get("/user", async ({ headers, set, jwt }) => {
+    const token = headers.authorization?.split(" ")[1];
+    const payload = await jwt.verify(token);
 
-        } catch (error) {
-            console.error('Error fetching all orders:', error);
-            set.status = 500;
-            return { message: 'Internal Server Error while retrieving order list.' };
-        }
-    })
+    if (!payload) {
+      set.status = 401;
+      return { error: "Unauthorized" };
+    }
 
-// GET /orders/:id
+    const userId = payload.id;
 
-    .get("/:id", async ({ headers, set, jwt }) => {
-        const token = headers.authorization?.split(" ")[1];
-        const payload = await jwt.verify(token)
+    try {
+      const query = `SELECT * FROM orders WHERE user_id = ${userId};`;
 
-        if (!payload) {
-            set.status = 401;
-            return { error: "Unauthorized" };
-        }
+      const [rows] = await dbPool.execute<mysql.RowDataPacket[]>(query);
 
-        const userId = payload.id;
+      if (rows.length === 0) {
+        set.status = 200;
+        return { message: "Orders not found" };
+      }
 
-        try {
-            const query = `SELECT * FROM orders WHERE user_id = ${userId};`;
+      const userOrdersList = rows.map((row) => ({
+        id: row.order_id,
+        user_id: row.user_id,
+        order_date: row.order_date,
+        status: row.status,
+        date_fulfilled: row.date_fulfilled,
+        price: row.price,
+        offer_id: row.offer_id,
+        description: row.description,
+      }));
 
-            const [rows] = await dbPool.execute<mysql.RowDataPacket[]>(query);
-
-            if (rows.length === 0) {
-                set.status = 200;
-                return { message: "Orders not found" };
-            }
-
-            const userOrdersList = rows.map((row) => ({
-                id: row.order_id,
-                user_id: row.user_id,
-                order_date: row.order_date,
-                status: row.status,
-                date_fulfilled: row.date_fulfilled,
-                price: row.price,
-                offer_id: row.offer_id,
-                description: row.description,
-            }));
-
-            set.status = 200;
-            return userOrdersList;
-
-        } catch (error) {
-            console.error("Error fetching user orders: ", error);
-            set.status = 500;
-            return { message: "Internal Server Error while retrieving user orders."};
-        }
-    })
+      set.status = 200;
+      return userOrdersList;
+    } catch (error) {
+      console.error("Error fetching user orders: ", error);
+      set.status = 500;
+      return { message: "Internal Server Error while retrieving user orders." };
+    }
+  });
