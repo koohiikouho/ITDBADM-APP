8b6aa232db0b2cd40d8a0fd03bf8324338c29f4b
koohiikouho <115139996+koohiikouho@users.noreply.github.com>
2025-11-16 21:26:20 +0800

    feat: add offers page

---
+++ itdbadm-frontend/src/pages/offers.tsx
@@ -0,0 +1,10 @@
+import OffersPage from "@/components/Offers";
+import DefaultLayout from "@/layouts/default";
+
+export default function SentOfferPage() {
+  return (
+    <DefaultLayout>
+      <OffersPage />
+    </DefaultLayout>
+  );
+}
